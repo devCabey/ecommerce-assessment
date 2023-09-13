2a79a85ec0503821dc9ebf1a7b1be1f4cf206a19
Caleb <agyakojo4321@gmail.com>
2023-09-13 10:31:59 +0000

    type definition done

--- engine/src/graphql/typeDefs.ts
+++ engine/src/graphql/typeDefs.ts
@@ -1,46 +1,36 @@
 const typeDefs = `
+  type Product {
+    id: String!
+    name: String!
+    price: Float!
+    phote: String
+    description: String
+  }
 
-type Product{
-  id:String!
-  name:String!
-  price:Float!
-  phote:String
-  description:String
-}
+  type Order {
+    product: Product
+    quantity: Int
+  }
 
-type Order{
-  product:Product
-  quantity:Int
-}
+  input CreateOrderInput {
+    productId: String
+    quantity: Int
+  }
 
-input ProductInput{
-  name:String!
-  price:Float!
-  description:String
-  photo:String!
-}
+  input UpdateOrderInput {
+    productId: String
+    quantity: Int
+  }
 
-input CreateOrderInput{
-  productId:String
-  quantity:Int
-}
-
-input UpdateOrderInput{
-  productId:String
-  quantity:Int
-}
-
-
-type Query{
-  getProduct:Product
-  getProducts:[Product]!
-  getOrders:[Order]!
-}
-type Mutation{
-  createOrder(input:CreateOrderInput!):[Order]
-  updateOrder(input:UpdateOrderInput!):[Order]
-}
-
+  type Query {
+    getProduct: Product
+    getProducts: [Product]!
+    getOrders: [Order]!
+  }
+  type Mutation {
+    createOrder(input: CreateOrderInput!): [Order]
+    updateOrder(input: UpdateOrderInput!): [Order]
+  }
 `;
 
 export default typeDefs;
