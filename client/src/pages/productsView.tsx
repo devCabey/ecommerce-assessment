7cf2dec7185712928f0b4688cb4efaf62d04ac08
Caleb <agyakojo4321@gmail.com>
2023-09-18 21:30:44 +0000

    filter component done

--- client/src/pages/productsView.tsx
+++ client/src/pages/productsView.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ProductItem from '../components/productItem'
 import ProductShortcut from '../components/productShortcut'
 import { filters, productsData } from '../data'
@@ -9,17 +9,23 @@ interface ProductsViewProps {
 }
 
 const ProductsView: React.FC<ProductsViewProps>=() =>{
+  const [select,setSelect] = useState<string>("")
   return (
     <div className='relative'>
       
      <div className='flex items-center justify-between px-10 bg-slate-100 sticky top-12 z-40'>
-        {filters.map((data)=><ProductShortcut key={data.filter} filter={data.filter} photo={data.photo}/>)}
+        <ProductShortcut filter="All Products" photo="https://images.ctfassets.net/2yd1b0rk61ek/10Wn9tj66w3iyrdkyiO4FV/bc8ba2617988ed55d34857f47e0e4130/apple_herobanner_en.jpg" onClick={()=>setSelect("All Products")} active={select}/>
+        {filters.map((data)=><ProductShortcut key={data.filter}  filter={data.filter} photo={data.photo} onClick={()=>setSelect(data.filter)} active={select}/>)}
      
      </div>
-     <h1 className='my-10 mx-10 text-xl font-bold '>"ALL PRODUCT"</h1>
+     <h1 className='my-10 mx-10 text-xl font-bold '>"{select}"</h1>
      <div className='grid grid-cols-4'>
         {
-          productsData.map((data)=><ProductItem key={data.id} name={data.name} photo={data.photo} id={data.id} description={data.description} price={data.price}/>)
+          productsData.map((data)=>{
+            if(data.name.includes(select) || select==="All Products"){
+             return <ProductItem key={data.id} name={data.name} photo={data.photo} id={data.id} description={data.description} price={data.price}/>
+            }else{return ""}
+          })
         }
      </div>
     </div>
