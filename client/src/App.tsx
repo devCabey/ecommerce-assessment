37884647a043692c9c4fe68b36262250bff245cf
Caleb <agyakojo4321@gmail.com>
2023-09-20 10:55:32 +0000

    apollo client setup done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ApolloClient, InMemoryCache,ApolloProvider } from '@apollo/client';
+
 import HomeView from './pages/homeView';
 import ProductsView from './pages/productsView';
 import ProductView  from './pages/productView';
@@ -7,20 +9,26 @@ import CheckoutView from './pages/checkoutView';
 import productLoader from './api/product';
 import Layout from './layout/layout';
 
-
+const client = new ApolloClient({
+	uri:'http://localhost:5000/graphql',
+	cache: new InMemoryCache()
+})
 
 function App() {
   return (
-		<BrowserRouter>
-			<Layout>
-				<Routes>
-					<Route path='/' element={<HomeView/>} />
-					<Route path= 'products' element={<ProductsView />}/>
-					<Route path= 'products/:id' element={<ProductView  loader={productLoader}/>}/>
-					<Route path= 'checkout' element={<CheckoutView />}/>
-				</Routes>
-			</Layout>
-		</BrowserRouter>
+		<ApolloProvider client={client}>
+			<BrowserRouter>
+						<Layout>
+							<Routes>
+								<Route path='/' element={<HomeView/>} />
+								<Route path= 'products' element={<ProductsView />}/>
+								<Route path= 'products/:id' element={<ProductView  loader={productLoader}/>}/>
+								<Route path= 'checkout' element={<CheckoutView />}/>
+							</Routes>
+						</Layout>
+			</BrowserRouter>
+		</ApolloProvider>
+		
   );
 }
 
